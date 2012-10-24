10eb86dbaee0b35ffe79cc3d3ea9e5c184999aa2
Jayden Ma <jayden@codebean.sg>
2012-10-24 17:30:35 +0800

    Initialized Vanilla Codebean Prototype

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VanillaCodebean::Application.config.secret_token = '0c0477d4cecffb9716d609640f94211a4eb89c895883dd6d3b986ea8b8eebd27d2208376771092cc54576af436e43e22466a072fb97bb18a01f50442805e613c'
